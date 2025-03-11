dad6c5486be60b4d33f3afa7b111d6875309b7a4
Ubhumma <devonejohnson04@gmail.com>
2025-03-11 15:37:41 -0400

    cors issue resolved

--- UpmeetEventSystemAPI/UpmeetEventSystemAPI/Program.cs
+++ UpmeetEventSystemAPI/UpmeetEventSystemAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<UpMeetEventDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddCors(options => { options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,5 +27,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseCors("AllowAll");
 app.Run();
