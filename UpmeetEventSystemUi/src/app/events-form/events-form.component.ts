436aff771c29a5aa961852348683eec8660c396a
Ubhumma <devonejohnson04@gmail.com>
2025-03-12 00:32:08 -0400

    minor CSS changes

--- UpmeetEventSystemUi/src/app/events-form/events-form.component.ts
+++ UpmeetEventSystemUi/src/app/events-form/events-form.component.ts
@@ -1,6 +1,7 @@
 
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-events-form',
@@ -9,5 +10,7 @@ import { Component } from '@angular/core';
   styleUrl: './events-form.component.css'
 })
 export class EventsFormComponent {
+
+  constructor(apiService: ApiService){};
 
 }
